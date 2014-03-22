591649d97632ddb14a550c3e4d072e773fb6856c
cenmong <cenmong@gmail.com>
2014-03-22 16:40:53 +0800

    add 15 minute granularity. datetime xaxis to be done.

--- anlz.py
+++ anlz.py
@@ -1,78 +1,12 @@
 from patricia import *
 from netaddr import *
 from env import *
+import os
 
-'''
-f4 = open('/datarv2/rib.20140310.1000', 'r')#IPv4 RIB
-f6 = open('/datarv6/rib.20140310.1000', 'r')#IPv6 RIB
-
-as6 = []
-#store IPv6 ASes into as6
-for line in f4.readlines():
-
-asds = []
-#store DS ASes into asds
-for line in f6.readlines():
-f4.close()
-f6.close()
-'''
-
-'''
-trie4 = trie(None)
-#build IPv4 trie
-f = open('pfx2as/routeviews-rv2-20140310-1200.pfx2as', 'r')#IPv4 prefix2as
-s = f.readline().split()
-s_addr = IPAddress(s[0]).bits()
-s_addr = s_addr.replace('.', '')
-s_addr = s_addr[:int(s[1])]
-
-while 1:
-    trie4[s_addr] = s[2]
-    try:
-        s = f.readline().split()
-        s_addr = IPAddress(s[0]).bits()
-        s_addr = s_addr.replace('.', '')
-        s_addr = s_addr[:int(s[1])]
-    except:
-        break
-f.close()
-
-trie6 = trie(None)
-#build IPv6 trie
-f = open('pfx2as/routeviews-rv6-20140310-0000.pfx2as', 'r')#IPv6 prefix2as
-s = f.readline().split()
-s_addr = IPAddress(s[0]).bits()
-s_addr = s_addr.replace(':', '')
-s_addr = s_addr[:int(s[1])]
-
-while 1:
-    trie6[s_addr] = s[2]
-    try:
-        s = f.readline().split()
-        s_addr = IPAddress(s[0]).bits()
-        s_addr = s_addr.replace(':', '')
-        s_addr = s_addr[:int(s[1])]
-    except:
-        break
-f.close()
-'''
-# Basic idea: attribute every update to its origin AS (the last one on the AS
-# path)
-
-# From 0000 to 2345, 15 as interval
-'''
-time = []
-firsthalf = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09']
-for i in range(10, 25):
-    firsthalf.append(str(i))
-secondhalf = ['00', '15', '30', '45']
-for fh in firsthalf:
-    for sh in secondhalf:
-        time.append(fh + sh)
-'''
 ym = yearmonth[0]
 dn = yearmonth[1]#number of days
 
+######variables with daily granularity#####
 as4all = []# Statistics of the whole month
 as6all = []
 asbothall = []
@@ -86,16 +20,24 @@ udn6 = {}
 
 ymdlist = []# Year+month+day strings
 
+######variables with 15 minutes granularity#####
+as4c15 = []# To save memory, use list instead of dict
+as6c15 = []
+ud4c15 = []
+ud6c15 = []
+dtlist = []# Date time list
+
 # TODO: If output files exist, use them instead of read all files again
 
 #IPv6
-filelist = 'metadata/6files' + ym 
-#filelist = 'metadata/test6files' + ym 
+#filelist = 'metadata/6files' + ym 
+filelist = 'metadata/test6files' + ym 
 flist = open(filelist, 'r')
-
 for line in flist.readlines():
     print line
     ymd = line.split('.')[-3]
+    datetime = line.split('.')[-3] + line.split('.')[-2]
+    dtlist.append(datetime)
     if ymd not in ymdlist:
         ymdlist.append(ymd)
         as6[ymd] = []# Initialize a list corresponding to a date
@@ -104,10 +46,14 @@ for line in flist.readlines():
         udn6[ymd] = 0
         asboth[ymd] = []
 
+    udcount = 0
+    ascount = 0
+    aslist = []
     f = open(hdname + line.replace('\n', ''), 'r')
     for line in f.readlines():
         if line == '' or line == '\n':
             udn6[ymd] += 1
+            udcount += 1
             continue
         header = line.split(': ')[0]
         try:
@@ -118,21 +64,31 @@ for line in flist.readlines():
             asn = content.split()[-1]
             if asn not in as6[ymd]:
                 as6[ymd].append(asn)
+            if asn not in aslist:
+                aslist.append(asn)
+                ascount += 1
             if asn not in as6all:
                 as6all.append(asn)
+
+    ud6c15.append(udcount)
+    as6c15.append(ascount)
     f.close()
 #IPv4
-filelist = 'metadata/4files' + ym 
-#filelist = 'metadata/test4files' + ym 
+#filelist = 'metadata/4files' + ym 
+filelist = 'metadata/test4files' + ym 
 flist = open(filelist, 'r')
 for line in flist.readlines():
     print line
     ymd = line.split('.')[-3]
 
+    udcount = 0
+    ascount = 0
+    aslist = []
     f = open(hdname + line.replace('\n', ''), 'r')
     for line in f.readlines():
         if line == '' or line == '\n':
             udn4[ymd] += 1
+            udcount += 1
             continue
         header = line.split(': ')[0]
         try:
@@ -143,8 +99,14 @@ for line in flist.readlines():
             asn = content.split()[-1]
             if asn not in as4[ymd]:
                 as4[ymd].append(asn)
+            if asn not in aslist:
+                aslist.append(asn)
+                ascount += 1
             if asn not in as4all:
                 as4all.append(asn)
+
+    ud4c15.append(udcount)
+    as4c15.append(ascount)
     f.close()
 
 for ymd in ymdlist:
@@ -161,6 +123,7 @@ print 'len(as6all)=',len(as6all)
 print 'len(asbothall)=',len(asbothall)
 
 # Store date: ASN information into output files to accelerate future run
+'''
 fo6 = open('output/6output' + ym, 'a')
 fo4 = open('output/4output' + ym, 'a')
 fob = open('output/boutput' + ym, 'a')
@@ -182,7 +145,7 @@ for ymd in ymdlist:
 fo6.close()
 fo4.close()
 fob.close()
-
+'''
 # Plot something
 as4count = []
 as6count = []
@@ -192,11 +155,17 @@ bin6ratio = []
 updates4 = []
 updates6 = []
 
-xaxis = []
+xaxis = []# Day granularity
 i = 0
 for ymd in ymdlist:
     i += 1
     xaxis.append(i)
+
+xaxis2 = []# 15 minutes granularity
+i = 0
+for dt in dtlist:
+    i += 1
+    xaxis2.append(i)
 
 for ymd in ymdlist:
     as4count.append(len(as4[ymd]))
@@ -204,20 +173,48 @@ for ymd in ymdlist:
     asbothcount.append(len(asboth[ymd]))
     bin6ratio.append(float(len(asboth[ymd]))/float(len(as6[ymd])) * 100)
     updates4.append(udn4[ymd])
-    updates6.append(udn6[ymd])
+    updates6.append(udn6[ymd]) 
 
-# TODO: store every thing into a file
+# store every thing into a file
+if not os.path.exists('output/plotdata' + ym):
+    f = open('output/plotdata' + ym, 'a')
+    f.write('as4count:')
+    for i in range(0, len(ymdlist)):
+        f.write(',' + str(as4count[i]))
+    f.write('\n')
+    f.write('as6count:')
+    for i in range(0, len(ymdlist)):
+        f.write(',' + str(as6count[i]))
+    f.write('\n')
+    f.write('asbothcount:')
+    for i in range(0, len(ymdlist)):
+        f.write(',' + str(asbothcount[i]))
+    f.write('\n')
+    f.write('bin6ratio:')
+    for i in range(0, len(ymdlist)):
+        f.write(',' + str(bin6ratio[i]))
+    f.write('\n')
+    f.write('updates4:')
+    for i in range(0, len(ymdlist)):
+        f.write(',' + str(updates4[i]))
+    f.write('\n')
+    f.write('updates6:')
+    for i in range(0, len(ymdlist)):
+        f.write(',' + str(updates6[i]))
+    f.write('\n')
+    f.close()
 
 import matplotlib.pyplot as plt 
 import numpy as np
 
 # definitions for the axes
-left = 0.04
+left = 0.05
 width = 0.92
 bottom = 0.15
 height = 0.8
 rect_scatter = [left, bottom, width, height]
 
+############################################by day as number
 plt.figure(1, figsize=(16, 12))
 
 axScatter = plt.axes(rect_scatter)
@@ -231,6 +228,7 @@ for i, txt in enumerate(as6count):
     axScatter.annotate(txt, (i + 1, as6count[i]))
 for i, txt in enumerate(asbothcount):
     axScatter.annotate(txt, (i + 1, asbothcount[i]))
+
 axScatter.set_xlabel('Date')
 axScatter.set_ylabel('Number of origin ASes')
 axScatter.set_yscale('log')
@@ -241,6 +239,7 @@ axScatter.set_ylim( (0, ylim) )
 plt.xticks(xaxis, ymdlist, rotation=70)
 plt.show()
 
+###########################################by day update number
 plt.figure(2, figsize=(16, 12))
 
 axScatter = plt.axes(rect_scatter)
@@ -259,4 +258,37 @@ ylim = np.max(updates4) + 100
 axScatter.set_ylim( (0, ylim) )
 
 plt.xticks(xaxis, ymdlist, rotation=70)
+plt.show()
+
+##########################################by 15 minutes as number
+plt.figure(3, figsize=(16, 12))
+
+axScatter = plt.axes(rect_scatter)
+axScatter.plot(xaxis2, as4c15, 'r-')
+axScatter.plot(xaxis2, as6c15, 'b-')
+# TODO: asbothc15
+
+axScatter.set_xlabel('Datetime')
+axScatter.set_ylabel('Number of origin ASes')
+
+ylim = np.max(as4c15) + 100
+axScatter.set_ylim( (0, ylim) )
+
+#plt.xticks(xaxis, ymdlist, rotation=70)
+plt.show()
+
+###########################################by 15 minutes update number
+plt.figure(4, figsize=(16, 12))
+
+axScatter = plt.axes(rect_scatter)
+axScatter.plot(xaxis2, ud4c15, 'r-')
+axScatter.plot(xaxis2, ud6c15, 'b-')
+
+axScatter.set_xlabel('Datetime')
+axScatter.set_ylabel('Number of updates')
+
+ylim = np.max(ud4c15) + 100
+axScatter.set_ylim( (0, ylim) )
+
+#plt.xticks(xaxis, ymdlist, rotation=70)
 plt.show()
